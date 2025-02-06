35d6276ef26cc0f8d4f58c732ee04a1b0131d8d3
YuraJian <110766853+YuraJian@users.noreply.github.com>
2025-02-06 12:17:17 +0200

    Delete pre-diploma practice directory

--- pre-diploma practice/script/script.js
+++
@@ -1,5 +0,0 @@
-$('.company__position').on('click', function() {
-    $(this).children('.company__position-text').slideToggle(300);
-    $(this).toggleClass('company__position-bg-color');
-    $(this).children('.company__position-info').children('.company__position-arrow').toggleClass('company__position-arrow-rotate');
-})
